b604779330367185dfe5e201ec1481640d47bb1c
benrabiaafaf <benrabiaafaf@gmail.com>
2020-02-15 23:23:57 +0100

    Update DBManager.scala
    logic tested and corrected

--- src/main/scala/io/swagger/server/Actors/DBManager.scala
+++ src/main/scala/io/swagger/server/Actors/DBManager.scala
@@ -7,6 +7,8 @@ import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import io.swagger.server.Actors.DBManager.{get_all_properties, get_property, post_comment_to_proprty, post_property}
 import io.swagger.server.model.{Comment, Commented_property, Simple_property}
 import spray.json.JsonParser.ParsingException
+
+import scala.Option
 import scala.math.BigDecimal
 
 
@@ -57,34 +59,44 @@ class DBManager extends Actor with ActorLogging {
 
   override def receive: Receive = {
 
-    case post_comment_to_proprty(propertyId, comment) => ???
+    case post_comment_to_proprty(propertyId, comment) => post_comment_to_proprty(propertyId,comment)
     case post_property(simple_property) => post_property(simple_property)
     case get_all_properties() => sender() ! get_all_properties()
-    case get_property(propertyId) => sender()! get_property(propertyId)
+    case get_property(propertyId) => sender()! get_property(propertyId).getOrElse(None)
   }
 
   def get_all_properties(): List[Simple_property] = {
     val commented_properties = read_properties()
-    val simple_propertie = commented_properties.map(_.property)
-    return simple_propertie
+    val simple_properties = commented_properties.map(_.property)
+    log.info(simple_properties.toString)
+    return simple_properties
   }
 
-  def get_property(propertyId: String): Commented_property  ={
+  def get_property(propertyId: String): Option[Commented_property]  ={
     val commented_properties = read_properties()
     val requested_property = commented_properties.find( item => item.property.property_id == propertyId)
-    return requested_property.get
+    log.info(requested_property.getOrElse(None).toString)
+    return requested_property
   }
 
   def post_property(simple_property: Simple_property)={
-    var properties = read_properties()
-    save_properties( properties ::: List(new Commented_property(property = simple_property, comments = Option(Nil))))
+    //TODO : Check weather property_id exists or not
+    val properties = read_properties()
+    val updated_properties = properties ++: List(new Commented_property(property = simple_property, comments = Option(Nil)))
+    log.info(updated_properties.toString)
+    save_properties(updated_properties)
   }
-}
 
-object Main {
-  def main(args: Array[String]): Unit = {
-    val system = ActorSystem()
-    val dBManager = system.actorOf(DBManager())
-    dBManager ! "test"
+  def post_comment_to_proprty(propertyId : String, comment: Comment): Unit ={
+    //TODO : check weather user has already posted a comment for the specified year
+    val commented_properties = read_properties()
+    val index = commented_properties.indexWhere( item => item.property.property_id == propertyId)
+    if ( index != -1){
+      val item = commented_properties(index)
+      val updated_item = item.copy(comments = Option(item.comments.getOrElse(Nil) ++: List(comment)))
+      val updated_commented_properties = commented_properties.take(index) ++: List(updated_item) ++: commented_properties.takeRight(commented_properties.length-index-1)
+      log.info(updated_commented_properties.toString)
+      save_properties(updated_commented_properties)
+    }
   }
 }
