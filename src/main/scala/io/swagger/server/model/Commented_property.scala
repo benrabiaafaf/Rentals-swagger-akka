50b8315c4253b3453aa69d9a14aaca0c80f67fc7
benrabiaafaf <benrabiaafaf@gmail.com>
2020-02-15 21:42:33 +0100

    Update Commented_property.scala
    roll back commentsed_property to origin

--- src/main/scala/io/swagger/server/model/Commented_property.scala
+++ src/main/scala/io/swagger/server/model/Commented_property.scala
@@ -6,11 +6,6 @@ package io.swagger.server.model
  * @param comments 
  */
 case class Commented_property ( property: Simple_property,
-                                var comments: Option[List[Comment]]
+                                comments: Option[List[Comment]]
                               )
-{
-  def add_comment(comment: Comment): Unit ={
-    comments.getOrElse(Nil) ++: List(comment)
-  }
-}
 
